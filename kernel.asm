495c540d2342053530a3beb43680a2b350c3279a
Andrew Teall <>
2020-11-11 13:56:19 -0500

    Switch to decrement based scanline count

--- kernel.asm
+++ kernel.asm
@@ -1,19 +1,24 @@
-     processor 6502
-     include "includes/vcs.h"
-     include "includes/macro.h"
-     SEG
-     ORG $F000
+        processor 6502
+        include "includes/vcs.h"
+        include "includes/macro.h"
+;Start Program
+        SEG.U vars
+        ORG $80
+temp ds 1
+Label1 .byte 3
+
+        SEG
+        ORG $F000
 
 PATTERN           = $80 ; storage Location (1st byte in RAM)    
 Reset
-    ldx #0
-    txa
+        ldx #0
+        txa
 Clear
         dex
         txs
         pha
         bne Clear
-
 
         ldx #0
         stx COLUBK         ; set the background color
@@ -52,30 +57,34 @@ VerticalBLank
 
 --------------------------------------------------------------------
 ; 192 scanlines of picture...
-        ldx #0                ; this counts our scanline number
+        ldx #192                ; this counts our scanline number
         ldy #%11111111
         sty PF0
         sty PF1
         sty PF2
 Top8Lines
-        inx
+        dex
         sta WSYNC
-        cpx #8
+        cpx #184
         bne Top8Lines
 
 
 MiddleLines
-        inx
-        ldy #0
-        sty PF1
-        sty PF2
-        ldy #%00010000
-        sty PF0
-        cpx #170
-        bne Sprite
-        ldy #%11111111
-        sty GRP0
-        sta RESP0
+        dex                                             ; 2
+        ldy #0                                          ; 2
+        sty PF1                                         ; 3
+        sty PF2                                         ; 3
+        ldy #%00010000                                  ; 2
+        sty PF0                                         ; 3
+        cpx #8                                        ; 2
+        bne Sprite                                      ; 3
+        ldy 4                                           ; 2
+HorizontalMove
+        ;dey
+        ;bne HorizontalMove
+        ;ldy #%11111111
+        ;sty GRP0
+        ;sta RESP0
 Sprite 
         ldy #0
         sty GRP0
@@ -83,18 +92,18 @@ Sprite
         ;ldy #%00001000
         ;sty HMP0
         ;sta HMOVE
-        cpx #184
+        cpx #8
         bne MiddleLines
 
 
 Bottom8Lines
-        inx
+        
         sta WSYNC
         ldy #%11111111
         sty PF0
         sty PF1
         sty PF2
-        cpx #192
+        dex
         bne Bottom8Lines
 
 ;-------------------------------------------------------------------------------
