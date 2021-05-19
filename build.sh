ed98b1b98024ac81f5b9e9488db1c864a7170d13
Andrew Teall <>
2021-05-19 12:02:32 -0400

    Updated build file with symbols

--- build.sh
+++ build.sh
@@ -2,6 +2,13 @@
 NAME=$(echo $1 | cut -d'.' -f1)
 LIST=$2
 if [ -n "$LIST" ]; then
-  LIST=-l$NAME.txt
+  LIST=-l$NAME.lst
+  SYM=-s$NAME.sym
 fi
-./dasm $1 $LIST -f3 -v5 -o$NAME.bin
+dasm $1 $LIST $SYM -f3 -v5 -o$NAME.bin
+mkdir -p bin
+mv $NAME.bin bin/
+if [ -n "$LIST" ]; then
+  mv $NAME.lst bin/
+  mv $NAME.sym bin/
+fi
