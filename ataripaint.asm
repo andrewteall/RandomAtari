665c712dba94a09e0a6585f3a7320dfc98240078
Andrew Teall <>
2021-06-05 22:09:00 -0400

    Make Global Constants Area

--- ataripaint.asm
+++ ataripaint.asm
@@ -5,11 +5,38 @@
 ; TODO: Add Labels under controls to display usage
 
 ; TODO: Finalize Colors and Decor and Name
-        
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Global Constants ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; End Global Constants ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ONE_COPY                        = #0
+TWO_COPIES_CLOSE                = #1
+TWO_COPIES_MEDIUM               = #2
+THREE_COPIES_CLOSE              = #3
+TWO_COPIES_WIDE                 = #4
+DOUBLE_SIZE_PLAYER              = #5
+THREE_COPIES_MEDIUM             = #6
+QUAD_SIZED_PLAYER               = #7
+MISSLE_SIZE_ONE_CLOCK           = #0
+MISSLE_SIZE_TWO_CLOCKS          = #16
+MISSLE_SIZE_FOUR_CLOCKS         = #32
+MISSLE_SIZE_EIGHT_CLOCKS        = #48
+
+P1_JOYSTICK_UP                  = #%00000001
+P1_JOYSTICK_DOWN                = #%00000010
+P1_JOYSTICK_LEFT                = #%00000100
+P1_JOYSTICK_RIGHT               = #%00001000
+P0_JOYSTICK_UP                  = #%00010000
+P0_JOYSTICK_DOWN                = #%00100000
+P0_JOYSTICK_LEFT                = #%01000000
+P0_JOYSTICK_RIGHT               = #%10000000
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Constants ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;PATTERN                        = $80                   ; storage Location (1st byte in RAM)
 TITLE_H_POS                     = #57
 TRACKDISPLAY_LEFT_H_POS         = #20
 TRACKDISPLAY_RIGHT_H_POS        = #68
@@ -35,29 +62,6 @@ BACKGROUND_COLOR                = #155          ; #155
 TITLE_COLOR                     = #132          ; #132
 CONTROLS_COLOR                  = #123          ; #123
 SELECTION_COLOR                 = #9            ; #9
-
-ONE_COPY                        = #0
-TWO_COPIES_CLOSE                = #1
-TWO_COPIES_MEDIUM               = #2
-THREE_COPIES_CLOSE              = #3
-TWO_COPIES_WIDE                 = #4
-DOUBLE_SIZE_PLAYER              = #5
-THREE_COPIES_MEDIUM             = #6
-QUAD_SIZED_PLAYER               = #7
-MISSLE_SIZE_ONE_CLOCK           = #0
-MISSLE_SIZE_TWO_CLOCKS          = #16
-MISSLE_SIZE_FOUR_CLOCKS         = #32
-MISSLE_SIZE_EIGHT_CLOCKS        = #48
-
-P1_JOYSTICK_UP                  = #%00000001
-P1_JOYSTICK_DOWN                = #%00000010
-P1_JOYSTICK_LEFT                = #%00000100
-P1_JOYSTICK_RIGHT               = #%00001000
-P0_JOYSTICK_UP                  = #%00010000
-P0_JOYSTICK_DOWN                = #%00100000
-P0_JOYSTICK_LEFT                = #%01000000
-P0_JOYSTICK_RIGHT               = #%10000000
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; End Constants ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
