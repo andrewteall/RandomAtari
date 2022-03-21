37a5fc2eb2f69392c01b1f31cdfe96a302e26240
andrewteall <andrewteall@gmail.com>
2022-03-21 16:46:40 -0400

    Remove format 1(not sure what it was used for)

--- utils/formatTrack.c
+++ utils/formatTrack.c
@@ -63,63 +63,8 @@ int main(int argc, char *argv[]) {
                     }
                 }
             }
-            if (argc == 3 && (strcmp(argv[2], "-f1")) == 0) {
-                tmp1 = 0;
-                while ((c = fgetc(trackFile)) && c != EOF) {
-                    if (c != ' ' ){
-                        if (c >= '0' && c <= '9') {
-                                c = c-'0';
-                                note[index++] = c;
-                                tmp1++;
-                                // printf("%d ", c);
-                        } else if (c == ',' || c == '\n') {
-                            if (tmp1 == 2) {
-                                note[index-2] = note[index-2]*10 + note[index-1];
-                                index--;
-                            }
-                            tmp1 = 0;
-                        }
-                        // printf("%d / %d\n", tmp1, index);
-                        if (index >= 4 && tmp1 != 2 && c != '\n' && c != ',') {
-                            // printf("$%d,",note[0]); //v
-                            // printf("$%d,",note[1]); //c
-                            // printf("$%d,",note[2]); //f
-                            // printf("$%d,",note[3]); //d
-
-                            tmp1 = lookupDuration(note[3]) & 7;
-                            tmp2 = note[2] << 3;
-                            tmp1 = tmp2 | tmp1;
-                            printf("$%x,",tmp1);
-
-
-                            tmp1 = note[0] << 4;
-                            tmp2 = note[1] & 15;
-                            tmp1 = tmp2 | tmp1;
-                            printf("$%x,",tmp1);
-                            index = 0;
-                        }
-                    } else {
-                        tmp1 = 0;
-                    }       
-                }
-                // printf("$%d,",note[0]);
-                // printf("$%d,",note[1]);
-                // printf("$%d,",note[2]);
-                // printf("$%d,",note[3]);
-                tmp1 = lookupDuration(note[3]) & 7;
-                tmp2 = note[2] << 3;
-                tmp1 = tmp2 | tmp1;
-                printf("$%x,",tmp1);
-
-
-                tmp1 = note[0] << 4;
-                tmp2 = note[1] & 15;
-                tmp1 = tmp2 | tmp1;
-                printf("$%x,$0,$0",tmp1);
-            }
-            
-            // -f or Format 2 converts https://alienbill.com/2600/atari-riff-machine/ or batari basic output
-            if (argc == 3 && (strcmp(argv[2], "-f2")) == 0) { 
+            // -f or Format 1 converts https://alienbill.com/2600/atari-riff-machine/ or batari basic output
+            if (argc == 3 && (strcmp(argv[2], "-f1")) == 0) { 
                 tmp1 = 0;
                 int numLen = 0;
                 char noteNums[3];
@@ -168,7 +113,6 @@ int main(int argc, char *argv[]) {
                         printf("$%x,",tmp1);
                     }
                 }
-
                 printf("\nNote Durations:\n");
                 for(int i=0; i<8;i++){
                     printf(".byte $%x\n",durationList[i]);
@@ -179,7 +123,6 @@ int main(int argc, char *argv[]) {
             return 1;
         }
         fclose(trackFile);
-        
         }
     return 0;
 }
